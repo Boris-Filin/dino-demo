b5121a985b92c0643c663e8f223e413d82ddf4b4
Boris Filin <boris.filin.2510@gmail.com>
2026-08-13 17:09:25 +1000

    remove everything

--- src/main/java/dino/shared/GameView.java
+++
@@ -1,9 +0,0 @@
-package dino.shared;
-
-public interface GameView {
-    void render(GameState state);
-
-    void setJumpHandler(Runnable handler);
-
-    void setRestartHandler(Runnable handler);
-}
